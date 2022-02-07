922f588896f7db7e0b14c931ab402c7ca9d52c74
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-08 01:12:57 +0530

    added-checking attendance

--- EmployeeWageUsingConstructor.java
+++ EmployeeWageUsingConstructor.java
@@ -6,9 +6,17 @@ public class EmployeeWageUsingConstructor{
 
 		System.out.println("Welcome to Employee Wage Problem using Constructor!!!!");
 	}
+	EmployeeWageUsingConstructor(int isFullTime) {
+		
+		double empCheck = Math.floor (Math.random() * 10) % 2;
+		if (empCheck == isFullTime)
+			System.out.println("Employee is Present");
+		else        
+			System.out.println("Employee is Absent");
+	}
 	public static void main(String[] args) {
 
 		EmployeeWageUsingConstructor emp = new EmployeeWageUsingConstructor();
-
+		emp = new EmployeeWageUsingConstructor(1);
 	}
 }
