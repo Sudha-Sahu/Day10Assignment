4b4a4c00c5dc2821bd4f22873768497b23cc4d5f
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-08 01:04:30 +0530

    Added- welcome to employee wage day 10 problem

---
+++ EmployeeWageUsingConstructor.java
@@ -0,0 +1,14 @@
+package com.Bridgelabz.Day10Assignment;
+
+public class EmployeeWageUsingConstructor{
+
+	EmployeeWageUsingConstructor() {
+
+		System.out.println("Welcome to Employee Wage Problem using Constructor!!!!");
+	}
+	public static void main(String[] args) {
+
+		EmployeeWageUsingConstructor emp = new EmployeeWageUsingConstructor();
+
+	}
+}
